35d2d3e843044aa0e6e387e6e17dac4b4ec97ffb
smart2004 <115572648+smart2004@users.noreply.github.com>
2023-06-08 10:29:30 +0300

    Add files via upload

---
+++ buildings.py
@@ -0,0 +1,41 @@
+# 86806313
+"""This code calculates farness from empty plots."""
+from typing import List, Tuple
+
+
+def nearest_buildings(buildings_qty: int, buildings_row: List[int]) -> int:
+    """Nulls set & 3 farness direction ways detection from nulls."""
+    proximity = [buildings_qty] * buildings_qty
+    null = [i for i in range(buildings_qty) if buildings_row[i] == 0]
+    init_null = null[0]
+    last_null = null[-1]
+    for i in range(init_null, buildings_qty):
+        if buildings_row[i] == 0:
+            proximity[i] = 0
+        else:
+            proximity[i] = proximity[i - 1] + 1
+    for i in range(last_null, init_null, -1):
+        if buildings_row[i] == 0:
+            proximity[i] = 0
+        else:
+            proximity[i] = min(proximity[i], proximity[i + 1] + 1)
+    for i in range(init_null - 1, -1, -1):
+        proximity[i] = proximity[i + 1] + 1
+    return proximity
+
+
+def read_input() -> Tuple[int, List[int]]:
+    """Input building quantity & building row."""
+    buildings_qty = int(input())
+    buildings_row = list(map(int, input().strip().split()))
+    return buildings_qty, buildings_row
+
+
+def init():
+    """Intro & output."""
+    buildings_qty, buildings_row = read_input()
+    print(*nearest_buildings(buildings_qty, buildings_row))
+
+
+if __name__ == '__main__':
+    init()
---
+++ hands_agility.py
@@ -0,0 +1,36 @@
+# 86808301
+"""This code calculates points in hands agility game."""
+from typing import Tuple
+
+
+def keys_vs_numbers(sixteen_cells: Tuple[int], keys: int) -> int:
+    """Numbers vs key coincide & points calculation function."""
+    set_row = [0] * 10
+    for ind in sixteen_cells:
+        if ind != '.':
+            set_row[int(ind)] = set_row[int(ind)] + 1
+    points = 0
+    for num in set_row:
+        if int(num) <= 2*keys and int(num) > 0:
+            points = points + 1
+    return points
+
+
+def read_input() -> Tuple[int, Tuple[int]]:
+    """Input available keys number for each player & gameplay field."""
+    keys = int(input())
+    sixteen_cells = []
+    for _ in range(4):
+        input_num = input().strip()
+        sixteen_cells.extend(tuple(input_num))
+    return sixteen_cells, keys
+
+
+def init():
+    """Intro & output."""
+    sixteen_cells, keys = read_input()
+    print(keys_vs_numbers(sixteen_cells, keys))
+
+
+if __name__ == '__main__':
+    init()
